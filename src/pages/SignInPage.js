a259af3780dcf4232ed94cf25589e6fdf8f3326d
DinhDuong <dinhduong1704bg@gmail.com>
2023-06-18 02:20:40 +0700

    add base signin and signup form

---
+++ src/pages/SignInPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SignInForm = () => {
+    return(
+        <></>
+    );
+}
+export const SignInPage = ({ history }) => (
+    <div className={`flex h-full`}>
+        <div className={`w-64 m-auto`}>
+            <SignInForm history={history} />
+        </div>
+    </div>
+);
