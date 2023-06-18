47d5e39312c03bca3878b3d1b72a4f9e390b743f
duongnn <laserbean4@gmail.com>
2023-06-18 16:30:40 +0700

    refactor

--- src/utils/state-provider.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { withRouter } from 'react-router-dom';
-import { AuthUserContext } from './auth-user-context';
-import { ROUTES } from '../constants';
-
-export const withAuthentication = (Component) => (props) => {
-    const [authUser, setAuthUser] = useState(null);
-}
