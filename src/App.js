36f934b6c11303038656b17444e7066022da1783
DinhDuong <dinhduong1704bg@gmail.com>
2023-06-23 20:04:38 +0700

    Add new code BoardPage and BoardsPage

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import { AuthUserContext } from './auth/auth-user-context';
 import { SignUpPage } from './pages/SignUpPage';
 import { SignInPage } from './pages/SignInPage';
 import { NotFoundPage } from './pages/NotFoundPage';
+import { BoardsPage } from './pages/BoardsPage';
+import { BoardPage } from './pages/BoardPage';
 
 export const Content = withAuthentication(() => (
     <Router>
@@ -21,6 +23,8 @@ export const Content = withAuthentication(() => (
                     <Redirect exact from="/" to="/boards" />
                     <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
                     <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+                    <Route exact path={ROUTES.BOARDS} component={BoardsPage} />
+                    <Route exact path={ROUTES.BOARD} component={BoardPage} />
                     <Route component={NotFoundPage} />
                 </Switch>
             </div>
